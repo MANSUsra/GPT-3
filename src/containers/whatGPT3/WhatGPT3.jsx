5f2c139355e6c22e5bc999250d9f3ac84189994d
MANSUsra <yousramansour2019@gmail.com>
2024-05-22 21:16:54 +0100

    Practicing React Frontend

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './whatGPT3.css'
+import { Feature } from '../../components'
+const WhatGPT3 = () => {
+  return (
+    <div className='gpt3__whatgpt3 section_margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-features'>
+        <Feature title="What is GPT-3" text="we so opinion friends me message as delight. Whole front do of plan.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do a plate heard oh ought."/>
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity lul. Friendly bachelor entrance to on by. As impossible own apartments"/>
+        <Feature title="Education" text=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
